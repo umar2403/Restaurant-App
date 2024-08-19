81a20eebbd67f2077c2de939979f0a1f3c72da54
Syed Umar <ee21b147@smail.iitm.ac.in>
2024-08-19 17:12:18 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react';
+import RestaurantList from './components/RestaurantList';
+import DishesMenu from './components/DishesMenu';
+import Cart from './components/Cart';
+import { RestaurantContext } from './contexts/RestaurantContext';
+import './App.css';  // Import the CSS file
 
-function App() {
+const App = () => {
+  const { selectedRestaurant } = useContext(RestaurantContext);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1 className="header">GFG Restaurant App</h1>
+        <Cart style={{ position: "absolute", right: "20px", top: "20px" }} />
+        <RestaurantList />
+        {selectedRestaurant && <DishesMenu />}
+      </div>
+    </>
   );
-}
+};
 
 export default App;
